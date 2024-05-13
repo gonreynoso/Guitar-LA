891f1d6424e02026d8da5f86063c7db5d89bd12f
gonreynoso <gonzalo.reynoso9@gmail.com>
2024-05-13 17:12:23 -0300

    state derivado

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,40 @@ import { useState } from "react";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+
+    const itemExist = cart.findIndex(guitar => item.id === guitar.id)
+    if (itemExist >= 0 ) { //existe en el carrito
+      const updatedCart = [...cart]
+      updatedCart[itemExist].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart , item])
+    }
+
+
+  }
 
   return (
+
+
+
     <>
-      <Header />
+      <Header
+      cart={cart} 
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
           {data.map((guitar) => (
             <Guitar 
             key={guitar.id}
-            guitar={guitar}/>
+            guitar={guitar}
+            addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,7 @@
-const Guitar = ({ guitar }) => {
+const Guitar = ({ guitar , addToCart}) => {
+
   const { id, name, image, description, price } = guitar;
 
-  const handleClick = (guitar) => {
-    console.log("click");
-  };
 
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -21,9 +19,7 @@ const Guitar = ({ guitar }) => {
         <button
           type="button"
           className="btn btn-dark w-100"
-          onClick={() => {
-            handleClick(guitar);
-          }}
+          onClick={() => addToCart(guitar)}
         >
           Agregar al Carrito
         </button>
