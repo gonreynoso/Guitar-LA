c1c26fee135a8cca369cb962181a2a02777df95a
gonreynoso <gonzalo.reynoso9@gmail.com>
2024-05-14 12:57:56 -0300

    adding interaction with buttons increase quantity

--- src/App.jsx
+++ src/App.jsx
@@ -11,36 +11,46 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(item) {
-
-    const itemExist = cart.findIndex(guitar => item.id === guitar.id)
-    if (itemExist >= 0 ) { //existe en el carrito
-      const updatedCart = [...cart]
-      updatedCart[itemExist].quantity++
-      setCart(updatedCart)
+    const itemExist = cart.findIndex((guitar) => item.id === guitar.id);
+    if (itemExist >= 0) {
+      //existe en el carrito
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
     } else {
-      item.quantity = 1
-      setCart([...cart , item])
+      item.quantity = 1;
+      setCart([...cart, item]);
     }
-
-
   }
 
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
   return (
-
-
-
     <>
-      <Header
-      cart={cart} 
-      />
+      <Header cart={cart} removeFromCart={removeFromCart} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar 
-            key={guitar.id}
-            guitar={guitar}
-            addToCart={addToCart}
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              addToCart={addToCart}
+              increaseQuantity={increaseQuantity}
             />
           ))}
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMemo } from "react";
 
-const Header = ({ cart }) => {
+const Header = ({ cart, removeFromCart, increaseQuantity}) => {
   //State Derivado
   const isEmpty = useMemo(() => cart.length === 0, [cart])
   const cartTotal = useMemo(() =>
@@ -56,16 +56,19 @@ const Header = ({ cart }) => {
                             <td>{guitar.name}</td>
                             <td className="fw-bold">${guitar.price}</td>
                             <td className="flex align-items-start gap-4">
-                              <button type="button" className="btn btn-dark">
+                              <button type="button" className="btn btn-dark" >
                                 -
                               </button>
                               {guitar.quantity}
-                              <button type="button" className="btn btn-dark">
+                              <button type="button" className="btn btn-dark" onClick={() => increaseQuantity(guitar.id)}>
                                 +
                               </button>
                             </td>
                             <td>
-                              <button className="btn btn-danger" type="button">
+                              <button 
+                              className="btn btn-danger" 
+                              type="button"
+                              onClick={() => removeFromCart(guitar.id)}>
                                 X
                               </button>
                             </td>
